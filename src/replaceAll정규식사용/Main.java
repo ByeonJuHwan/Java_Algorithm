8f0166aa27ea694698a67dd41920cc66488e9a08
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2023-01-22 14:02:46 +0900

    replaceAll 정규식사용

---
+++ src/replaceAll정규식사용/Main.java
@@ -0,0 +1,21 @@
+package replaceAll정규식사용;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.nextLine();
+        System.out.println(T.solution(str));
+
+    }
+
+    private String solution(String str) {
+        String answer = "NO";
+        str = str.toUpperCase().replaceAll("[^A-Z]", "");
+        String tmp = new StringBuilder(str).reverse().toString();
+        if(str.equals(tmp)) answer = "YES";
+        return answer;
+    }
+}
