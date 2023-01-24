5754f5d261a90f0d8ea5c2cbca3fea8de35bc3ee
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2023-01-24 11:20:14 +0900

    빈문자열 추가로 예외 해결하기

---
+++ src/문자열압축/Main.java
@@ -0,0 +1,30 @@
+package 문자열압축;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
+    }
+
+    private String solution(String str) {
+        String answer = "";
+        str = str + " "; // 여기서 문자열하나들 더안주면 i와 i+1을 비교할때 마지막 인덱스에서 예외가 발생한다.
+        int cnt = 1;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                cnt++;
+            }else{
+                answer += str.charAt(i);
+                if (cnt > 1) {
+                    answer += String.valueOf(cnt);
+                }
+                cnt = 1;
+            }
+        }
+        return answer;
+    }
+}
