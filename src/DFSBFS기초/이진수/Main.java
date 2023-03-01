fd3fd1ed66df18d418950e04149dfa4104b203e1
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2023-03-01 23:22:57 +0900

    피보나치 재귀로하는법

---
+++ src/DFSBFS기초/이진수/Main.java
@@ -0,0 +1,19 @@
+package DFSBFS기초.이진수;
+
+public class Main {
+    // 재귀함수는 이제부터 DFS로 표현
+    public void DFS(int n) {
+        // 재귀함수는 일단 무조건 If-else문으로
+        if (n== 0) return;
+        else {
+            // 재귀함수는 스택 프레임으로
+            DFS(n / 2);
+            System.out.print((n % 2) + " ");
+        }
+    }
+
+    public static void main(String[] args)  {
+        Main T = new Main();
+        T.DFS(11);
+    }
+}
